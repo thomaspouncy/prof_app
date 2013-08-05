2d9e4c430104c35af9674ff7470e25ce0c325260
thomas.pouncy@gmail.com <thomas.pouncy@gmail.com>
2013-08-05 16:45:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfApp::Application.config.secret_token = 'a06fbdca40095099dcdd3ccc17aac08ad35fed185193b67f6bd8860e297a71c96cdd87ea9c6eed2e745747a1157026eb4618ed5cd98b995f8679bbe942c1f1cd'
